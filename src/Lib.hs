25bea3c320942fe7934b023e9676d4ffcf78e78c
Phat Le <ltanphat@gmail.com>
2017-10-10 05:05:40 +0700

    Parse to Gsearch and Guser.
    Somehow Vim indent wrong format, so code can not compile. :(

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,51 +1,55 @@
 #!/usr/bin/env stack
 -- stack script --resolver lts-8.22
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
 module Lib
     ( displayUsers
     ) where
 
-import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Lazy as B
+
 import qualified Data.Map as Map
 
 import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
 import           Data.Aeson
 import           Data.Text (Text)
+import           Network.HTTP.Conduit (simpleHttp)
+import GHC.Generics
+
 
 
 data Guser = Guser { login :: !Text
-											, avatar_url :: !Text
-										}
-data Gsearch = Gsearch { items :: [Guser] }
+                    , avatar_url :: !Text
+										} deriving (Generic, Show)
 
-initRequest :: String -> Request
-initRequest url = do
-	initialRequest <- parseRequest 
-	return initialRequest
-		{ method = "GET"
-			, requestHeaders =
+instance FromJSON Guser
+instance ToJSON Guser
+
+data Gsearch = Gsearch { items :: [Guser] } deriving (Generic, Show)
+instance FromJSON Gsearch
+instance ToJSON Gsearch
+
+jsonURL :: String
+jsonURL = "https://api.github.com/search/users?q=location:singapore"
+
+getJSON :: IO B.ByteString
+getJSON = do
+  manager <- newManager $ managerSetProxy noProxy tlsManagerSettings
+  initRequest <- parseRequest jsonURL
+  let request = initRequest { requestHeaders =
 				[ ("Content-Type", "application/json; charset=utf-8")
 				  , ("User-Agent", "hser")
 				]
 		}
+  res <- httpLbs request manager
+  return $ responseBody res
 
-
-fetchUser :: String -> IO (Either String Gsearch)
-fetchUser url = do
-	manager <- newManager $ managerSetProxy noProxy tlsManagerSettings
-	setGlobalManager manager
-	request <- initRequest(url)
-	withResponse request manager $ \res -> do
-		return $ eitherDecode $ responseBody res
 
 displayUsers :: IO ()
 displayUsers = do
-  value <- fetchUser url
-  case value of
+  d <- (eitherDecode <$> getJSON) :: IO (Either String Gsearch)
+  case d of
     Left err -> putStrLn err
-    Right gs -> print gs
-  where
-    url = "https://api.github.com/search/users?q=location:singapore"
+    Right ps -> print ps
 
