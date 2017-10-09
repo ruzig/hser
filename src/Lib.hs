656adece0ed103f3fc080623eea4974d2e7878c2
Phat Le <ltanphat@gmail.com>
2017-10-09 17:58:14 +0700

    Can fetch json. Finally

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,30 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-8.22
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( displayUsers
     ) where
 
+import qualified Data.ByteString.Lazy.Char8 as L8
+import           Network.HTTP.Client
+import           Network.HTTP.Client.TLS
+import           Network.HTTP.Simple
+
 displayUsers :: IO ()
-displayUsers = putStrLn "awesome Github"
+displayUsers = do
+    manager <- newManager $ managerSetProxy noProxy tlsManagerSettings
+    setGlobalManager manager
+
+    initialRequest <- parseRequest "https://api.github.com/search/users?q=location:singapore"
+    let request = initialRequest
+            { method = "GET"
+            , requestHeaders =
+                [ ("Content-Type", "application/json; charset=utf-8"),
+                ("User-Agent", "hser")
+                ]
+            }
+    response <- httpLBS request
+
+    L8.putStrLn $ getResponseBody response
+
