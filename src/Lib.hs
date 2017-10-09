4e630933e4d0c4503dc741400592d45f60a029cf
Phat Le <ltanphat@gmail.com>
2017-10-09 21:28:43 +0700

    Wrong types :(

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,29 +7,45 @@ module Lib
     ) where
 
 import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.Map as Map
 
 import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
-import           Data.Aeson.Parser           (json)
-import           Network.HTTP.Client.Conduit (bodyReaderSource)
-import           Data.Conduit                (($$))
-import           Data.Conduit.Attoparsec     (sinkParser)
+import           Data.Aeson
+import           Data.Text (Text)
+
+
+data Guser = Guser { login :: !Text
+											, avatar_url :: !Text
+										}
+data Gsearch = Gsearch { items :: [Guser] }
+
+initRequest :: String -> Request
+initRequest url = do
+	initialRequest <- parseRequest 
+	return initialRequest
+		{ method = "GET"
+			, requestHeaders =
+				[ ("Content-Type", "application/json; charset=utf-8")
+				  , ("User-Agent", "hser")
+				]
+		}
+
+
+fetchUser :: String -> IO (Either String Gsearch)
+fetchUser url = do
+	manager <- newManager $ managerSetProxy noProxy tlsManagerSettings
+	setGlobalManager manager
+	request <- initRequest(url)
+	withResponse request manager $ \res -> do
+		return $ eitherDecode $ responseBody res
 
 displayUsers :: IO ()
 displayUsers = do
-	manager <- newManager $ managerSetProxy noProxy tlsManagerSettings
-	setGlobalManager manager
-
-	initialRequest <- parseRequest "https://api.github.com/search/users?q=location:singapore"
-	let request = initialRequest
-					{ method = "GET"
-					, requestHeaders =
-							[ ("Content-Type", "application/json; charset=utf-8"),
-							("User-Agent", "hser")
-							]
-					}
-	withResponse request manager $ \response -> do
-		value <- bodyReaderSource (responseBody response)
-					$$ sinkParser json
-		print value
+  value <- fetchUser url
+  case value of
+    Left err -> putStrLn err
+    Right gs -> print gs
+  where
+    url = "https://api.github.com/search/users?q=location:singapore"
 
